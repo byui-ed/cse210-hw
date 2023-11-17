43bf219c7db09f8715576439335a6c118101b9d3
byui-ed <bonconnigeria011@gmail.com>
2023-11-17 17:09:36 +0100

    dev03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        
+Console.WriteLine("proverbs 3:5-6");
+Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("proverbs 3:5");
     }
 }
