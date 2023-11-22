ca0e45795b338b40861cf285359f1d7f5554c4d0
byui-ed <bonconnigeria011@gmail.com>
2023-11-22 16:26:22 +0100

    pdev_4

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Who are people that you appreciate?...");
+
+Thread.Sleep(1000);
+
+Console.WriteLine("What are personal strengths of yours?");
+
+Console.Write("+");
+
+Thread.Sleep(500);
+
+Console.Write("\b \b");
+Console.Write("|");
+
+DateTime startTime = DateTime.Now;
+DateTime futureTime = startTime.AddSeconds(5);
+
+Thread.Sleep(3000);
+
+DateTime currentTime = DateTime.Now;
+if (currentTime < futureTime);
+
+
+
     }
+  
 }
