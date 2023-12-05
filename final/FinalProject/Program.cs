7c97da4ef88564bfdb87138f494b3bf4cdf626b0
byui-ed <bonconnigeria011@gmail.com>
2023-12-05 17:19:19 +0100

    open-ended changes

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,32 @@
-using System;
+using System.IO;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+    
+        Console.WriteLine("Welcome to the Membership Registration Program");
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+
+
+        Console.WriteLine($"Hello, {name}!");
+
+        Console.WriteLine("Would you like to become a member of the Church of Jesus Christ of Latter-day Saints? (yes/no)");
+        string response = Console.ReadLine();
+
+        if (response.ToLower() == "yes")
+        {
+            Console.WriteLine("Thank you for your interest in becoming a member!");
+        
+        }
+        else
+        {
+            Console.WriteLine("Thank you for considering it. If you change your mind, feel free to reach out!");
+        }
+
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+
     }
 }
---
+++ final/FinalProject/name.cs
@@ -0,0 +1,12 @@
+public class name
+{
+    public string _student;
+    public string _Bishop;
+    public string _President;
+    public string _age;
+    public string _sex;
+    public string _location;
+    public string _country;
+    public int _friends;
+
+}
